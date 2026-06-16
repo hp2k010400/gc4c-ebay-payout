8ba2955cc75f05974c0e3fbfc091eba987ae3108
Harry Phillips <harry.phillips@golfclubs4cash.co.uk>
2026-06-16 12:23:23 +0100

    Fix aggregation: correct signs, use totalFeeAmount for charges, ebayCollectedTaxAmount for tax

--- pages/api/payout.js
+++ pages/api/payout.js
@@ -94,46 +94,49 @@ async function fetchTransactions(accessToken, date) {
 }
 
 function aggregate(transactions) {
-  let sales = 0, refunds = 0, postage = 0, hold = 0, claim = 0
-  let adjustment = 0, other = 0, charge = 0
-  let fvfFix = 0, fvfVar = 0, vhiFee = 0, bspFee = 0, intFee = 0
-  let collectTax = 0, pp = 0
+  let sales = 0, refunds = 0, charges = 0, postage = 0, hold = 0, claim = 0
+  let adjustment = 0, other = 0, charge = 0, collectors_tax = 0
 
   for (const tx of transactions) {
     const net = parseFloat(tx.amount?.value || 0)
     const gross = parseFloat(tx.totalFeeBasisAmount?.value || tx.amount?.value || 0)
-    const type = tx.transactionType
+    const fees = parseFloat(tx.totalFeeAmount?.value || 0)
+    const isDebit = tx.bookingEntry === 'DEBIT'
 
-    if (type === 'SALE') {
-      sales += gross
-      pp += parseFloat(tx.shippingCharge?.value || 0)
-      for (const fee of tx.marketplaceFee || []) {
-        const amt = parseFloat(fee.amount?.value || 0)
-        if (fee.feeType === 'COLLECTOR_TAX') collectTax += amt
-        else if (fee.feeType === 'FINAL_VALUE_FEE_FIXED_PER_ORDER') fvfFix += amt
-        else if (fee.feeType === 'FINAL_VALUE_FEE_VARIABLE_PER_ORDER') fvfVar += amt
-        else if (fee.feeType === 'VERY_HIGH_ITEM_NOT_AS_DESCRIBED') vhiFee += amt
-        else if (fee.feeType === 'BELOW_STANDARD_PERFORMANCE_FEE') bspFee += amt
-        else if (fee.feeType === 'INTERNATIONAL_FEE') intFee += amt
-      }
-    } else if (type === 'REFUND') {
-      refunds += gross
-    } else if (type === 'SHIPPING_LABEL') {
-      postage += net
-    } else if (type === 'HOLD') {
-      hold += net
-    } else if (type === 'DISPUTE') {
-      claim += net
-    } else if (type === 'ADJUSTMENT') {
-      adjustment += net
-    } else if (type === 'OTHER') {
-      other += net
-    } else if (type === 'NON_SALE_CHARGE') {
-      charge += net
+    switch (tx.transactionType) {
+      case 'SALE':
+        sales += gross
+        charges -= fees
+        collectors_tax += parseFloat(tx.ebayCollectedTaxAmount?.value || 0)
+        break
+      case 'REFUND':
+        refunds -= gross
+        charges += fees  // fee credits returned on refund
+        break
+      case 'SHIPPING_LABEL':
+        postage -= net
+        break
+      case 'DISPUTE':
+        claim -= net
+        break
+      case 'CREDIT':
+        claim += net   // dispute reversal / credit
+        break
+      case 'NON_SALE_CHARGE':
+        charge -= net
+        break
+      case 'HOLD':
+        hold += isDebit ? -net : net
+        break
+      case 'ADJUSTMENT':
+        adjustment += isDebit ? -net : net
+        break
+      case 'OTHER':
+        other += isDebit ? -net : net
+        break
     }
   }
 
-  const charges = fvfFix + fvfVar + vhiFee + bspFee + intFee
   const total_payout = parseFloat((sales + refunds + charges + postage + hold + claim + adjustment + other + charge).toFixed(2))
 
   return {
@@ -148,8 +151,8 @@ function aggregate(transactions) {
     charge: parseFloat(charge.toFixed(2)),
     total_payout,
     difference: 0,
-    collectors_tax: parseFloat(collectTax.toFixed(2)),
-    postage_and_packaging: parseFloat(pp.toFixed(2)),
+    collectors_tax: parseFloat(collectors_tax.toFixed(2)),
+    postage_and_packaging: 0,
   }
 }
 
